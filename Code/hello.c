6366d49c679d72419e2a8e0459993f2350c007fd
Jatin <jatin.gupta@flipkart.com>
2020-01-16 14:36:21 +0530

    On branch F1-Branch  Changes to be committed: 	modified:   hello.c
    Implemented Feature1
    	Learning Branch

--- Code/hello.c
+++ Code/hello.c
@@ -5,9 +5,17 @@ int main()
 	int doSomething = 100;
 	printf("Hello World\n");
 	printf("Hello ken\n");
+	doBranching();
 	return 0;
 }
 
 void doSomething() {
 	
 }
+
+void doBranching() {
+
+	printf("Learning Branching\n");
+
+}
+
